a766ebc9fa0205872c33fc8a693d95d04dfa5087
Makali'i <makaliifernandez@gmail.com>
2022-06-08 19:56:18 -0700

    add ci scripts to deploy react vite to gh pages on push

---
+++ scripts/gh-pages-deploy.mjs
@@ -0,0 +1,32 @@
+import {execa} from 'execa';
+import * as fs from 'fs';
+
+
+  (async () => {
+    try {
+      await execa("git", ["checkout", "--orphan", "gh-pages"]);
+      console.log("Building...");
+      await execa("npm", ["run", "build"]);
+      // Understand if it's dist or build folder
+      const folderName = fs.existsSync("dist") ? "dist" : "build";
+
+      // # navigate into the build output directory
+      // cd dist
+
+      // # if you are deploying to a custom domain
+      // # echo 'example.com' > CNAME
+      // await execa('echo', ['makaliifernandez.com','./dist/CNAME'])
+
+      await execa("git", ["--work-tree", folderName, "add", "--all"]);
+      await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+      console.log("Pushing to gh-pages...");
+      await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+      await execa("rm", ["-r", folderName]);
+      await execa("git", ["checkout", "-f", "main"]);
+      await execa("git", ["branch", "-D", "gh-pages"]);
+      console.log("Successfully deployed");
+    } catch (e) {
+      console.log(e.message);
+      process.exit(1);
+    }
+  })();
